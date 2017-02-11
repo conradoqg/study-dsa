07d03b3a333b1ac48dc8f6503a934ba705fb0309
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-02-11 19:16:29 -0200

    Adds more big o examples.

--- src/bigOFunctions.js
+++ src/bigOFunctions.js
@@ -31,6 +31,20 @@
  **/
 
 /**
+ * Simple function that executes only one statement.
+ * Big O: O(1)
+ *   
+ * @memberOf BigOFunctions
+ * @param {number} n First number
+ * @param {number} m Second number
+ * @returns Multipliation of n by m
+ */
+function O1Simple(n, m) {
+    this.step('n', 'm');
+    return n * m; // 1 work
+}
+
+/**
  * Function that calculates the powers of 2.
  * Big O: O(log N)
  * 
@@ -52,17 +66,20 @@ function powers0f2(n) {
 }
 
 /**
- * Simple function that executes only one statement.
- * Big O: O(1)
- *   
+ * Function that checks if n is a prime number. It only needs to go up to the square root of N.
+ * Big O: O(vN)
+ * 
  * @memberOf BigOFunctions
- * @param {number} n First number
- * @param {number} m Second number
- * @returns Multipliation of n by m
+ * @param {number} n Number to check.
+ * @returns {boolean} If it is or not.
  */
-function O1Simple(n, m) {
-    this.step('n', 'm');
-    return n * m; // 1 work
+function isPrime(n) {
+    for (let x = 2; x <= Math.sqrt(n); x++) {
+        if (n % x == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /**
@@ -77,7 +94,37 @@ function ONSimple(array) {
 }
 
 /**
- * Simple function that executes one statement in a m * n combination.
+ * O(N) example function with 2 consecutives loops that executes one statement per array item.
+ * Big O: O(N+N) = O(N)
+ *   
+ * @memberOf BigOFunctions
+ * @param {array} array to loop trought.
+ */
+function ONAdd(array) {
+    array.forEach(() => this.step('array')); // N work
+    array.forEach(() => this.step('array')); // then N work
+}
+
+/**
+ * Function that calculates the factorial for number N.
+ * Big O: O(N)
+ * 
+ * @memberOf BigOFunctions
+ * @param {number} n Number to calcule factorial.
+ * @returns {number} Factorial calculated.
+ */
+function factorial(n) {
+    if (n < 0) {
+        return -1;
+    } else if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+/**
+     * O(N^2) example that executes one statement in a m * n combination.
  * Big O: O(N^2)
  *   
  * @memberOf BigOFunctions
@@ -90,7 +137,21 @@ function ON2Simple(array) {
 }
 
 /**
- * Simple function that executes one statement every 2 * n combination.
+ * O(N^2) example that has an array loop inside other array loop. The inner loop does half of the work of the outer loop
+ * Big O: O(N * N/2) = O((N^2)/2) = O(N^2)
+ * 
+ * @memberOf BigOFunctions
+ * @param {array} array array. 
+ */
+function ON2HalfInnerLoop(array) {
+    for (let i = 0; i < array.length; i++) { // N work        
+        for (let j = i + 1; j < array.length; j++) { // N/2 work (on average)                        
+        }
+    }
+}
+
+/**
+ * O(2^N) example that executes one statement every 2 * n combination.
  * Big O: O(2^N)
  *    
  * @memberOf BigOFunctions
@@ -105,7 +166,21 @@ function O2NSimple(n) {
 }
 
 /**
- * Function that calculates a fibonacci number.
+ * Prints all fibonacci numbers from 0 to N.
+ * Big O: O(N) * O(2^N) = O(2^N)
+ * 
+ * @memberOf BigOFunctions
+ * @param {number} n Upper number.
+ */
+function allFibonacci(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i + ': ' + fibonacci(i));
+    }
+}
+
+
+/**
+ * O(2^N) example that calculates a fibonacci number.
  * Big O: O(2^N)
  *    
  * @memberOf BigOFunctions
@@ -157,20 +232,6 @@ function ONXNSimple(arrayA, arrayB) {
 }
 
 /**
- * Simple function that has an array loop inside other array loop. The inner loop does half of the work of the outer loop
- * Big O: O(N * N/2) = O((N^2)/2) = O(N^2)
- * 
- * @memberOf BigOFunctions
- * @param {array} array array. 
- */
-function ON2HalfInnerLoop(array) {
-    for (let i = 0; i < array.length; i++) { // N work        
-        for (let j = i + 1; j < array.length; j++) { // N/2 work (on average)                        
-        }
-    }
-}
-
-/**
  * O(N) example that has an array loop, the loop is array size / 2.
  * Big O: O(N)
  * 
@@ -199,7 +260,26 @@ function sum(node) {
         return 0;
     }
     return sum(node.left) + node.value + sum(node.right); // 2^log N work = O(N) which 2^depth but each recursive call does log N work.
-} 
+}
+
+/**
+ * Prints all permutations for a given string.
+ * Big O: O(N * N * N!) = O(N^2 * N!)
+ * 
+ * @memberOf BigOFunctions
+ * @param {any} str String to permutate
+ * @param {string} [prefix=''] Substring to permutate (internal).
+ */
+function permutation(str, prefix = '') { // O(N!)
+    if (str.length() == 0) {
+        console.log(prefix); // O(1)
+    } else {
+        for (let i = 0; i < str.length(); i++) { // O(N)
+            let rem = str.substring(0, i) + str.substring(i + 1);
+            permutation(rem, prefix + str.charAt(i)); // O(N)
+        }
+    }
+}
 
 module.exports = {
     powers0f2,
